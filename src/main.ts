732c4d445a95e0a0f0f42cea7e436ee81a44e15e
Armaiic <bastiendandre.pro@gmail.com>
2023-09-20 22:29:35 +0200

    setp 3 finish

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,26 @@ const ctx = canvas.getContext("2d")!;
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+
 ctx.fillStyle = "#000000";
 ctx.fillRect(0, 0, width, height);
 
-ctx.fillStyle = "#ff0000";
-const circleRadius = 20; // Adjust the size of the circle here
-const circleX =  100 ; // Position the circle on the left
-const circleY = 250 ; // Position the circle at the top
-ctx.arc(circleX, circleY, circleRadius, 0, Math.PI * 2);
-ctx.fill();
+const dotcolor = "#ff0000";
+ctx.fillStyle = dotcolor ;
+const circleRadius = 50; // Adjust the size of the circle here
+ const  numberofdot = 4
+ 
+function makedot (x, y) {
+    ctx.fillStyle = dotcolor;
+    ctx.beginPath();
+    ctx.arc(x, y, circleRadius, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+for ( let i =0; i < numberofdot; i++ ) {
+    makedot(circleRadius, circleRadius);
+    makedot(canvas.width - circleRadius, circleRadius);
+     makedot(circleRadius, canvas.height - circleRadius);
+     makedot(canvas.width - circleRadius, canvas.height - circleRadius);
+}
+ 
