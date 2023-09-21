0a06daf8805d4d06e133ac56c8083e5acaf0cdd7
Armaiic <bastiendandre.pro@gmail.com>
2023-09-21 13:56:45 +0200

    step 4.1 completed

--- src/main.ts
+++ src/main.ts
@@ -1,74 +1,97 @@
 import "./style.css";
 
 const canvas = document.querySelector<HTMLCanvasElement>("#particules-canvas")!;
-
 const ctx = canvas.getContext("2d")!;
-
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
-
 const circleRadius = 50; // Adjust the size of the circle here
-const speedanimation = 2 ; // speed of the dot animation 
-const animationInterval = setInterval(draw, 10); // Adjust the interval for the desired animation speed
 
-ctx.fillStyle = "#000000";
-ctx.fillRect(0, 0, width, height);
+// Create an array to store the dots and their positions
+const dots: { x: number; y: number; vx: number; vy: number;couleur: string  }[] = [];
 
-const dotcolor = "#ff0000"; // put the color in  const  for change easy early the color
-ctx.fillStyle = dotcolor ;  
- // make the first dot of start 
-let dot1 = { x: circleRadius, y: circleRadius };
-let dot2 = { x: canvas.width - circleRadius, y: circleRadius };
-let dot3 = { x: circleRadius, y: canvas.height - circleRadius };
-let dot4 = { x: canvas.width - circleRadius, y: canvas.height - circleRadius };
-
- //function who make the dot  
-function makedot(x : number , y: number ) {
-    ctx.fillStyle = dotcolor;     
-    ctx.beginPath();
-    ctx.arc(x, y, circleRadius, 0, Math.PI * 2);
-    ctx.fill();
+// Randomly generate x and y values within the specified range
+function getRandomCoordinates() {
+  const x = Math.random() * width;
+  const y = Math.random() * height;
+  return { x, y };
 }
-//function who move the dot and stop if the dot arrive in the middle
-function draw() {
-    ctx.clearRect(0, 0, width, height); // clean the canevas of the present dot 
 
-    // part of the function who can move the 4 dot in diagonal
-    dot1.x += speedanimation;
-    dot1.y += speedanimation;
-  
-    dot2.x -= speedanimation;
-    dot2.y += speedanimation;
-  
-    dot3.x += speedanimation;
-    dot3.y -= speedanimation;
-  
-    dot4.x -= speedanimation;
-    dot4.y -= speedanimation;
-  
-    //call the function makedot for draw the dot
-  makedot(dot1.x, dot1.y);
-  makedot(dot2.x, dot2.y);
-  makedot(dot3.x, dot3.y);
-  makedot(dot4.x, dot4.y);
- 
-  //put in a variable the value of middle of the canevas 
-  const centerX = canvas.width / 2;
-  const centerY = canvas.height / 2;
-  if (
-    dot1.x >= centerX &&
-    dot2.x <= centerX &&
-    dot3.x >= centerX &&
-    dot4.x <= centerX &&
-    dot1.y >= centerY &&
-    dot2.y >= centerY &&
-    dot3.y <= centerY &&
-    dot4.y <= centerY
-  ) {
-    clearInterval(animationInterval);  //clean the interval of animation 
+function getRandomVelocity() {
+  const minSpeed = 1;
+  const maxSpeed = 4;
+  const vx =
+    (Math.random() * (maxSpeed - minSpeed) + minSpeed) *
+    (Math.random() < 0.5 ? -1 : 1);
+  const vy =
+    (Math.random() * (maxSpeed - minSpeed) + minSpeed) *
+    (Math.random() < 0.5 ? -1 : 1);
+  return { vx, vy };
+}
+
+// Function to generate a random hex color
+function getRandomHexColor(): string {
+  var randomcolor = "#";
+  var letters = "0123456789ABCDEF";
+  for (var i = 0; i < 6; i++) {
+    randomcolor += letters[Math.floor(Math.random() * 16)];
   }
+  return randomcolor;
 }
 
+// Updated makedot function to create and store dots
+function makedot() {
+  const randomCoordinates = getRandomCoordinates();
+  const { x, y } = randomCoordinates;
+  const { vx, vy } = getRandomVelocity();
+  const couleur = getRandomHexColor(); // Get a random color here
+  dots.push({ x, y, vx, vy, couleur });
+}
 
+// Function to move the dots 
+function moveDots() {
+  // Clear the canvas
+  ctx.clearRect(0, 0, width, height);
 
- 
+  dots.forEach((dot, index) => {
+    // Randomly change the dot's position within a small range
+    dot.x += dot.vx;
+    dot.y += dot.vy;
+
+    if (dot.x < 0 || dot.x > width) {
+      dot.vx *= -1;
+    }
+    if (dot.y < 0 || dot.y > height) {
+      dot.vy *= -1;
+    }
+
+    // Remove dots that are too far from the canvas to save memory
+    if (dot.x < 0 || dot.x > width || dot.y < 0 || dot.y > height) {
+      dots.splice(index, 1);
+    }
+  });
+
+  // Add new dots if the number of dots is below a certain threshold
+  if (dots.length < 100) {
+    makedot();
+  }
+
+  // Draw each dot
+  dots.forEach((dot) => {
+    ctx.fillStyle = dot.couleur; // Set the color from dot object
+    ctx.beginPath();
+    ctx.arc(dot.x, dot.y, circleRadius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+  });
+}
+
+// Start moving dots with setInterval
+setInterval(moveDots, 1000 / 60); // 60 FPS
+
+// Initial dot creation
+for (let i = 0; i < 100; i++) {
+  makedot();
+}
+
+// Initial draw
+moveDots();
